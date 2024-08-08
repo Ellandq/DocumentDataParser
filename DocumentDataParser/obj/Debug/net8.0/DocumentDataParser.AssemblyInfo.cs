33940bb85f0f01f9177e43f73dbf00c21d86e73d
Julia3254 <s28580@pjwstk.edu.pl>
2024-08-08 12:36:48 +0200

    Resolved merge conflicts.

--- DocumentDataParser/obj/Debug/net8.0/DocumentDataParser.AssemblyInfo.cs
+++ DocumentDataParser/obj/Debug/net8.0/DocumentDataParser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DocumentDataParser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51ddc1f9127fabfceb8a8dfa03c17652992882f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0d82e90760dc11c625daa313902594b0612b355")]
 [assembly: System.Reflection.AssemblyProductAttribute("DocumentDataParser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DocumentDataParser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
