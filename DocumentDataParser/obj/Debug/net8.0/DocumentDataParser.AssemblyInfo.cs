3a93975a06fff4d78a4bf1d76c1e4d128033ee90
Julia3254 <s28580@pjwstk.edu.pl>
2024-08-08 12:18:21 +0200

    Init Branch

--- DocumentDataParser/obj/Debug/net8.0/DocumentDataParser.AssemblyInfo.cs
+++ DocumentDataParser/obj/Debug/net8.0/DocumentDataParser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DocumentDataParser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea070c5648b12739cf2fc23b932ddf449d2b81d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51ddc1f9127fabfceb8a8dfa03c17652992882f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DocumentDataParser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DocumentDataParser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
